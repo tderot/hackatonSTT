13978f0d6faa7c0ed921e2d10a2797930fe21565
Tandav81 <davidtanguy@gmail.com>
2017-03-23 19:51:13 +0100

    rajout button

--- request.php
+++ request.php
@@ -5,8 +5,17 @@
 include 'header.php';
 ?>
 
-<h1>Séléction de produit à éliminer</h1>
+<h1>C'est qui qui a mangé des trucs trop gras et qui culpabilise?</h1>
 
+<table class="table table-bordered">
+    <thead>
+    <th>nom</th>
+    <th>kcal</th>
+    <th>note</th>
+    <th>image</th>
+    <th>celui-ci</th>
+    </thead>
+    <tbody>
 <?php
 
 for ($j=1; $j<5; $j++){
@@ -21,20 +30,6 @@ $result = file_get_contents($url);
 
 $json = json_decode($result, true);
 
-
-?>
-
-
-<table class="table table-bordered">
-    <thead>
-    <th>nom</th>
-    <th>kcal</th>
-    <th>note</th>
-    <th>image</th>
-    <th>celui-ci</th>
-    </thead>
-    <tbody>
-    <?php
     for ($i = 0; $i < 100; $i++) {
         if (isset($json['products'][$i]['product_name']) && isset($json['products'][$i]['nutriments']['energy_value']) && isset($json['products'][$i]['nutrition_grades']) && isset($json['products'][$i]['image_url'])) {
 
@@ -49,6 +44,10 @@ $json = json_decode($result, true);
             echo '<td>' . $json['products'][$i]['nutrition_grades'] . '</td>';
 
             echo '<td>' . '<img src="' . $image . '" alt="boire ou manger"></td>';
+
+            echo '<td><form action="sport.php" method="get">
+                      <button type="submit" class="btn btn-success">c\'est à cause de lui</button>
+                    </form></td>';
 
             echo '</tr>';
         }
