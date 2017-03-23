2f27aa634b657b7875944ed109c8168bae428027
Slix1 <sylvain.jan2@gmail.com>
2017-03-23 15:03:03 +0100

    1st commit

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>OFF</title>
+</head>
+<body>
+<form action="request.php" method="POST">
+    <input type="text" class="form-control" placeholder="produit" name="produit">
+    <input type="submit" name="Ok" value="Find this produit !">
+</form>
+</body>
+</html>
