24a033774e2736d27099ddaf822f68fd41febd87
tderot <derot7@gmail.com>
2017-03-23 18:35:58 +0100

    rien

--- index.php
+++ index.php
@@ -10,26 +10,14 @@
 
 
 <body>
-<form action="request.php" method="post">
-<div class="btn-group">
-    <button type="button" class="btn btn-danger">Selectionne avec tes gros doigts</button>
-    <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span class="caret"></span>
-        <span class="sr-only">Toggle Dropdown</span>
-    </button>
-    <ul class="dropdown-menu" name="produit">
-        <li><a href="request.php">pizzas</a></li>
-        <li><a href="request.php">sodas</a></li>
-    </ul>
-</div>
-</form>
 
 
 
-<!--    <form action="request.php" method="POST">-->
-<!--        <input type="text" class="form-control" placeholder="produit" name="produit">-->
-<!--        <input type="submit" name="Ok" value="Find this produit !">-->
-<!--    </form>-->
+
+    <form action="request.php" method="POST">
+        <input type="text" class="form-control" placeholder="produit" name="produit">
+        <input type="submit" name="Ok" value="Find this produit !">
+    </form>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
