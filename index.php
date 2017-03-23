d06d0cd1d343f10a684773bc47e2c6dab27289c0
tderot <derot7@gmail.com>
2017-03-23 17:50:38 +0100

    mise en forme de la 1er page

--- index.php
+++ index.php
@@ -1,14 +1,39 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>OFF</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 </head>
+
+
 <body>
-<form action="request.php" method="POST">
-    <input type="text" class="form-control" placeholder="produit" name="produit">
-    <input type="submit" name="Ok" value="Find this produit !">
+<form action="request.php" method="post">
+<div class="btn-group">
+    <button type="button" class="btn btn-danger">Selectionne avec tes gros doigts</button>
+    <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <span class="caret"></span>
+        <span class="sr-only">Toggle Dropdown</span>
+    </button>
+    <ul class="dropdown-menu" name="produit">
+        <li><a href="request.php">pizzas</a></li>
+        <li><a href="request.php">sodas</a></li>
+    </ul>
+</div>
 </form>
+
+
+
+<!--    <form action="request.php" method="POST">-->
+<!--        <input type="text" class="form-control" placeholder="produit" name="produit">-->
+<!--        <input type="submit" name="Ok" value="Find this produit !">-->
+<!--    </form>-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
 </body>
 </html>
--- request.php
+++ request.php
@@ -1,37 +1,22 @@
 <?php
-//Default parameters by case
 
 
+$url = 'https://ssl-api.openfoodfacts.org/category/{product}.json';
 
-//Format url
-
-$url = 'https://fr-en.openfoodfacts.org/category/{product}.json';
-
-// Where we will set the value of the scan
 
 $produit = $_POST['produit'];
 
 $url = str_replace(['{product}'],[$produit],$url);
 
-// Connection to the API (french version here)
-
 $result = file_get_contents($url);
-
-// Decoding the JSON into an usable array (the value "true" confirms that the return is only an array)
 
 $json = json_decode($result, true);
 
-// Get the datas we want
 
-$productName = $json[$produit]['product_name'];
+for ($i=0; $i<25;$i++) {
 
-$brand = $json[$produit]['brands'];
-
-$image = $json[$produit]['image_small_url'];
-
-$viewData = file_get_contents('response.html');
-
-echo str_replace(
-    ['{productName}','{brand}','{image}','{json}'],
-    [$productName,$brand,$image,print_r($json,true)],
-    $viewData);
+    $img=$json ['products'][$i]['image_url'];
+    var_dump($json ['products'][$i]['product_name']);
+    var_dump($json ['products'][$i]['image_url']);
+    echo '<img src="'.$img.'"alt= "buvez">';
+}
