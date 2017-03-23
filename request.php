7423601bb2d19f620777d210385615bc83b31419
Tandav81 <davidtanguy@gmail.com>
2017-03-23 18:46:34 +0100

    on vire les produits vide

--- request.php
+++ request.php
@@ -1,10 +1,12 @@
 <?php
 
-$url = 'https://fr.openfoodfacts.org/category/{product}.json';
+for ($j=1; $j<5; $j++){
+$url = 'https://fr.openfoodfacts.org/category/{product}/' . $j . '.json';
+
 
 $produit = $_POST['produit'];
 
-$url = str_replace(['{product}'],[$produit],$url);
+$url = str_replace(['{product}'], [$produit], $url);
 
 $result = file_get_contents($url);
 
@@ -14,33 +16,38 @@ $json = json_decode($result, true);
 ?>
 <table>
     <thead>
-        <th>nom</th>
-        <th>kcal</th>
-        <th>note</th>
-        <th>image</th>
+    <th>nom</th>
+    <th>kcal</th>
+    <th>note</th>
+    <th>image</th>
     </thead>
     <tbody>
-<?php
-for ($i=0; $i<20; $i++)
-{
+    <?php
+    for ($i = 0; $i < 100; $i++) {
+        if (isset($json['products'][$i]['product_name']) && isset($json['products'][$i]['nutriments']['energy_value']) && isset($json['products'][$i]['nutrition_grades']) && isset($json['products'][$i]['image_url'])) {
 
-    $image=$json['products'][$i]['image_url'];
+            $image = $json['products'][$i]['image_url'];
 
-    echo '<tr>';
+            echo '<tr>';
 
-    echo '<td>' . $json['products'][$i]['product_name'] . '</td>';
+            echo '<td>' . $json['products'][$i]['product_name'] . '</td>';
 
-    echo '<td>' . $json['products'][$i]['nutriments']['energy_value'] . '</td>';
+            echo '<td>' . $json['products'][$i]['nutriments']['energy_value'] . '</td>';
 
-    echo '<td>' . $json['products'][$i]['nutrition_grades'] . '</td>';
+            echo '<td>' . $json['products'][$i]['nutrition_grades'] . '</td>';
 
-    echo '<td>' . '<img src="'.$image.'" alt="boire ou manger"></td>';
+            echo '<td>' . '<img src="' . $image . '" alt="boire ou manger"></td>';
 
-    echo '</tr>';
-}
+            echo '</tr>';
+        }
+    }
+    }
 ?>
     </tbody>
 </table>
+
+
+
 
 
 
