0f9b3e51a2b4decce2825e5ac5ce0afbbd91dba3
Tandav81 <davidtanguy@gmail.com>
2017-03-24 12:06:43 +0100

    flammes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'header.php';
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12 banner">
-                    <img src="image/medecine.png">
+                    <img src="image/medecin.jpg">
                 </div>
             </div>
         </div>
@@ -15,12 +15,15 @@ include 'header.php';
           <div class="row">
             <div class="col-md-offset-4 col-md-3 search">
                 <form action="request.php" method="POST">
-                    <input type="text" class="form-control" placeholder="produit" name="produit">
-                    <input type="submit" name="Ok" value="Find this product !">
+                    <label for="click">Merci de renseigner le fautif de vos prochaines douleurs!</label>
+                    <input type="text" class="form-control" placeholder="produit" name="produit" id="click">
+                    <input type="submit" id="Ok" value="Alors? C'est gras?">
                 </form>
             </div>
         </div>
     </header>
+    <img class="logoopen" src="image/openfoodfactslogo.png">
+    <img class="logowcs" src="image/logo_small_orange_horizontal.png">
 
 <?php
 include 'footer.php';
--- sport.php
+++ sport.php
@@ -25,62 +25,64 @@ switch ($productArray[2]) {
         break;
 }
 ?>
-<div class="container-fluid sportif">
-    <div class="row prod">
-        <div class="col-md-2">
-            <img class="imgProduct" src="<?php echo $productArray[3] ?>" alt="boire ou manger">
+    <div class="container-fluid sportif">
+        <div class="row prod">
+            <div class="col-md-2">
+                <img class="imgProduct" src="<?php echo $productArray[3] ?>" alt="boire ou manger">
+            </div>
+            <div class="col-md-2 texteSport">
+                <?php echo $productArray[2] ?>
+            </div>
+            <div class="col-md-2 texteSport">
+                <?php echo $productArray[0] ?>
+            </div>
+            <div class="col-md-2 texteSport">
+                <?php echo $productArray[1] ?> Kcal
+            </div>
+
         </div>
-        <div class="col-md-2 texteSport">
-            <?php echo $productArray[2] ?>
+
+        <div class="row">
+            <div class="col-md-2">
+                <a href="index.php"><img src="image/fleche.png" alt="retour acceuil"></a>
+            </div>
+
+            <div class="col-md-offset-2 col-md-8">
+
+                <form method="POST">
+
+                    <div class="form-group choix">
+                        <label for="sport">Choisissez un sport pour <br/>perdre toutes ces calories!</label>
+                        <select style="width: 20%" class="form-control" id="sport" name="sport">
+                            <?php
+
+                            $sport = mysqli_query($bdd, "SELECT * FROM sports");
+                            while ($data = mysqli_fetch_assoc($sport)) {
+                                $truc = $data['sport'];
+                                echo '<option value="' . $data['id'] . '">' . $truc . '</option>';
+                            }
+                            ?>
+
+                        </select>
+                    </div>
+                    <input type="submit" value="Bonne ambiance" class="btn btn-primary btn-lg" data-toggle="modal"
+                           data-target="#myModal"/>
+
+                </form>
+            </div>
         </div>
-        <div class="col-md-2 texteSport">
-            <?php echo $productArray[0] ?>
-        </div>
-    <div class="col-md-2 texteSport">
-        <?php echo $productArray[1] ?>  Kcal
     </div>
-
-    </div>
-
-    <div class="row">
-        <div class="col-md-2">
-            <a href="index.php"><img src="image/fleche.png" alt="retour acceuil"></a>
-        </div>
-
-        <div class="col-md-offset-2 col-md-8">
-
-            <form method="POST">
-
-                <div class="form-group choix">
-                    <label for="sport">Choisissez un sport pour <br/>perdre toutes ces calories!</label>
-                    <select style="width: 20%" class="form-control" id="sport" name="sport">
-                        <?php
-
-                        $sport = mysqli_query($bdd, "SELECT * FROM sports");
-                        while ($data = mysqli_fetch_assoc($sport)) {
-                            $truc = $data['sport'];
-                            echo '<option value="' . $data['id'] . '">' . $truc . '</option>';
-                        }
-                        ?>
-
-                    </select>
-                </div>
-                <input type="submit" value="Calcul" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal"/>
-
-            </form>
-        </div>
-    </div>
-</div>
 <?php
 
 $calprod = $productArray[1];
 $calcul = $truc;
 
 if (isset($_POST['sport'])) {
-    $res = mysqli_query($bdd, "SELECT kcal FROM sports where id=" . $_POST['sport']);
+    $res = mysqli_query($bdd, "SELECT * FROM sports where id=" . $_POST['sport']);
 
     while ($data = mysqli_fetch_assoc($res)) {
         $calorie = $data['kcal'];
+        $acti = $data['sport'];
         $heure = floor($calprod / $calorie);
         $minute = ((($calprod / $calorie) - $heure) * 60);
         $minute = ((($calprod / $calorie) - $heure) * 60);
@@ -91,28 +93,27 @@ if (isset($_POST['sport'])) {
 
 ?>
 
-<!-- Modal -->
-<div class="modal fade" tabindex="-1" role="dialog" id="myModal">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                            aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel"></h4>
-                <?php
-                if ($heure<1){
-                    echo '<img src="https://media.tenor.co/images/b50f78b7ccc3693c39a2ca2a10d83b7a/tenor.gif"class="coco" >';
+    <!-- Modal -->
+    <div class="modal fade" tabindex="-1" role="dialog" id="myModal">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel"></h4>
+                    <?php
+                    if ($heure < 1) {
+                        echo '<img src="https://media.tenor.co/images/b50f78b7ccc3693c39a2ca2a10d83b7a/tenor.gif"class="coco" >';
 
-                }
-                else{
-                    echo '<img  src="http://www.biblond.com/wp-content/uploads/2016/03/bloggif_56e837c2c175f.gif"class="coco">';
-                }
-                ?>
-            </div>
-            <div class="modal-body">
-                <?php
+                    } else {
+                        echo '<img  src="http://www.biblond.com/wp-content/uploads/2016/03/bloggif_56e837c2c175f.gif"class="coco">';
+                    }
+                    ?>
+                </div>
+                <div class="modal-body">
+                    <?php
 
-                echo 'Mais vous savez moi je ne crois pas qu’il y ait de bonne ou de mauvaise situation… Moi si je devais 
+                    echo 'Mais vous savez moi je ne crois pas qu’il y ait de bonne ou de mauvaise situation… Moi si je devais 
                 résumer ma vie aujourd’hui, avec vous, je dirai que c’est d’abord des rencontres, des gens qui m’ont tendu 
                 la main, peut-être à un moment où je ne pouvais pas, où j’étais seul chez moi. Et c’est assez curieux de se 
                 dire que les hasards, les rencontres forgent une destinée parce que quand on a le goût de la chose, quand on 
@@ -124,17 +125,16 @@ Et finalement quand beaucoup de gens aujourd’hui me disent : “Mais comment f
 répond très simplement, je leur dis que c’est ce goût de l’Amour, ce goût donc qui m’a poussé aujourd’hui à entreprendre une 
 construction mécanique mais demain qui sait, peut-être simplement à me mettre au service de la communauté, à faire le don, 
 le don de soi. <br/>
-Donc pour résumer je pense que vous devriez faire ' . $heure . ' heures et ' . round($minute) . ' minutes de '.$_POST['sport'];
+Donc pour résumer je pense que vous devriez faire :<br/><strong> ' . $heure . ' heures et ' . round($minute) . ' minutes de ' . $acti . '</strong>';
 
-                ?>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
 
 <?php
 
